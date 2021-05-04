28c419b18714160cb60db6f4f9c472446f82ebc1
limy-901 <ly.misty901@gmail.com>
2021-05-04 21:32:46 +0900

    Delete redundant files

--- limys/settings.py
+++ limys/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', "default_value")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
